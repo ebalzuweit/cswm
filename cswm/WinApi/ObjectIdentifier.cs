124180c6e21a0168601a8e0b73a2deb646b65eb6
Evan Balzuweit <ebalzuweit@gmail.com>
2022-07-29 10:59:52 -0500

    add winuser constants

---
+++ cswm/WinApi/ObjectIdentifier.cs
@@ -0,0 +1,9 @@
+namespace cswm.WinApi;
+
+/// <summary>
+/// <see href="https://docs.microsoft.com/en-us/windows/win32/winauto/object-identifiers"/>
+/// </summary>
+public enum ObjectIdentifier : int
+{
+    OBJID_WINDOW = 0
+}
