7035235dfb47845e8e63714bcf1b98b4c5b520a4
Evan Balzuweit <ebalzuweit@gmail.com>
2022-08-03 22:06:18 -0500

    add Rect

---
+++ cswm/WinApi/Rect.cs
@@ -0,0 +1,20 @@
+using System.Runtime.InteropServices;
+
+namespace cswm.WinApi;
+
+/// <summary>
+/// <see href="https://www.pinvoke.net/default.aspx/Structures/RECT.html"/>
+/// </summary>
+[StructLayout(LayoutKind.Sequential)]
+public struct Rect
+{
+    public readonly int Left, Top, Right, Bottom;
+
+    public Rect(int left, int top, int right, int bottom)
+    {
+        Left = left;
+        Top = top;
+        Right = right;
+        Bottom = bottom;
+    }
+}
