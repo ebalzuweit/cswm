d097f040329d049a74c6d452c3d72757cf0b9aef
Evan Balzuweit <ebalzuweit@gmail.com>
2022-08-07 12:44:39 -0500

    position windows

--- src/cswm/WindowManagement/WindowManagementService.cs
+++ src/cswm/WindowManagement/WindowManagementService.cs
@@ -4,6 +4,7 @@ using System.Text;
 using cswm.Events;
 using cswm.WinApi;
 using cswm.WindowManagement.Arrangement;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace cswm.WindowManagement;
@@ -11,16 +12,19 @@ namespace cswm.WindowManagement;
 public class WindowManagementService
 {
     private readonly ILogger? _logger;
+    private readonly WindowManagementOptions _options;
     private readonly WindowTrackingService _windowTrackingService;
     private readonly IArrangementStrategy _arrangementStrategy;
 
     public WindowManagementService(
         ILogger<WindowManagementService> logger,
+        IConfiguration configuration,
         MessageBus bus,
         WindowTrackingService windowTrackingService,
         IArrangementStrategy arrangementStrategy)
     {
         _logger = logger;
+        _options = configuration.GetSection(nameof(WindowManagementOptions)).Get<WindowManagementOptions>();
         _windowTrackingService = windowTrackingService ?? throw new ArgumentNullException(nameof(windowTrackingService));
         _arrangementStrategy = arrangementStrategy ?? throw new ArgumentNullException(nameof(arrangementStrategy));
     }
@@ -57,7 +61,27 @@ public class WindowManagementService
             )
         );
         var windowLayouts = _arrangementStrategy.Arrange(monitorLayouts);
-        // TODO move windows to positions
+        foreach (var layout in windowLayouts)
+            SetWindowPos(layout.Window, layout.Position);
+    }
+
+    private bool SetWindowPos(Window window, Rect position)
+    {
+        var windowsPadding = (window.Position.Width - window.ClientPosition.Width) / 2;
+        var adjustedPosition = new Rect(
+            left: position.Left - windowsPadding,
+            top: position.Top,
+            right: position.Right + windowsPadding,
+            bottom: position.Bottom + windowsPadding);
+
+        return User32.SetWindowPos(
+            window.hWnd,
+            HwndInsertAfterFlags.HWND_NOTOPMOST,
+            x: adjustedPosition.Left + _options.Padding,
+            y: adjustedPosition.Top + _options.Padding,
+            cx: adjustedPosition.Width - (_options.Padding * 2),
+            cy: adjustedPosition.Height - (_options.Padding * 2),
+            SetWindowPosFlags.SWP_ASYNCWINDOWPOS | SetWindowPosFlags.SWP_NOACTIVATE | SetWindowPosFlags.SWP_SHOWWINDOW);
     }
 
     private void OnWindowTrackingReset()
