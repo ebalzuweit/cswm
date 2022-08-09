eedf7ffd52ab9cabe32824dc644084c62a4f00c4
Evan Balzuweit <ebalzuweit@gmail.com>
2022-08-08 22:57:27 -0500

    UnhookWinEvent

--- src/cswm/WinApi/User32.cs
+++ src/cswm/WinApi/User32.cs
@@ -10,18 +10,37 @@ public static class User32
     public delegate void WinEventProc(IntPtr hWinEventHook, EventConstant eventType, IntPtr hwnd, ObjectIdentifier idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
 
     /// <summary>
-    /// <see href="https://www.pinvoke.net/default.aspx/user32.setwineventhook"/>
+    /// Sets an event hook function for a range of events.
     /// </summary>
-    /// <param name="eventMin"></param>
-    /// <param name="eventMax"></param>
-    /// <param name="hmodWinEventProc"></param>
-    /// <param name="lpfnWinEventProc"></param>
-    /// <param name="idProcess"></param>
-    /// <param name="idThread"></param>
-    /// <param name="dwFlags"></param>
-    /// <returns></returns>
+    /// <remarks>
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setwineventhook"/>
+    /// <para/>
+    /// <see href="https://www.pinvoke.net/default.aspx/user32.setwineventhook"/>
+    /// </remarks>
+    /// <param name="eventMin">Specifies the event constant for the lowest event value in the range of events that are handled by the hook function. This parameter can be set to EVENT_MIN to indicate the lowest possible event value.</param>
+    /// <param name="eventMax">Specifies the event constant for the highest event value in the range of events that are handled by the hook function. This parameter can be set to EVENT_MAX to indicate the highest possible event value.</param>
+    /// <param name="hmodWinEventProc">Handle to the DLL that contains the hook function at lpfnWinEventProc, if the WINEVENT_INCONTEXT flag is specified in the dwFlags parameter. If the hook function is not located in a DLL, or if the WINEVENT_OUTOFCONTEXT flag is specified, this parameter is NULL.</param>
+    /// <param name="lpfnWinEventProc">Pointer to the event hook function. For more information about this function, see WinEventProc.</param>
+    /// <param name="idProcess">Specifies the ID of the process from which the hook function receives events. Specify zero (0) to receive events from all processes on the current desktop.</param>
+    /// <param name="idThread">Specifies the ID of the thread from which the hook function receives events. If this parameter is zero, the hook function is associated with all existing threads on the current desktop.</param>
+    /// <param name="dwFlags">Flag values that specify the location of the hook function and of the events to be skipped.</param>
+    /// <returns>
+    /// If successful, returns an HWINEVENTHOOK value that identifies this event hook instance. Applications save this return value to use it with the <see cref="UnhookWinEvent"/> function.
+    /// If unsuccessful, returns zero.
+    /// </returns>
     [DllImport("user32.dll")]
     public static extern IntPtr SetWinEventHook(EventConstant eventMin, EventConstant eventMax, IntPtr hmodWinEventProc, WinEventProc lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+    /// <summary>
+    /// Removes an event hook function created by a previous call to <see cref="SetWinEventHook"/>.
+    /// </summary>
+    /// <remarks>
+    /// <see href="https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-unhookwinevent"/>
+    /// </remarks>
+    /// <param name="hWinEventHook">Handle to the event hook returned in the previous call to SetWinEventHook.</param>
+    /// <returns>If successful, returns TRUE; otherwise, returns FALSE.</returns>
+    [DllImport("user32.dll")]
+    public static extern bool UnhookWinEvent(IntPtr hWinEventHook);
 
     #region Monitor Functions
 
