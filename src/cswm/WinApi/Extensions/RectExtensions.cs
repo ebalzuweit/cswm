4ce15fddd3fc947ad9f2335eb9a028043f16951e
ebalzuweit <ebalzuweit@gmail.com>
2023-03-11 16:16:51 -0600

    Window arrangement refactor (#6)
    * refactor window arrangement, fix margin & padding issues
    
    * use named tuple
    
    * remove unncessarcy usings

--- src/cswm/WinApi/Extensions/RectExtensions.cs
+++ src/cswm/WinApi/Extensions/RectExtensions.cs
@@ -9,4 +9,39 @@ public static class RectExtensions
             return false;
         return true;
     }
+
+    public static (Rect Left, Rect Right, bool VerticalSplit) Split(this Rect rect, int margin = 0)
+    {
+        var verticalSplit = rect.Width >= rect.Height; // slightly prefer vertical splits ( LEFT | RIGHT ) to horizontal ( TOP | BOTTOM )
+        var dimension = verticalSplit ? rect.Width : rect.Height;
+        var midpoint = dimension / 2;
+        if (dimension % 2 == 1)
+            midpoint += 1; // left split gets the extra
+        var leftMid = rect.Left + midpoint;
+        var topMid = rect.Top + midpoint;
+        // we give the left partition the extra 1/2 margin
+        return verticalSplit
+            ? (
+                new Rect(rect.Left, rect.Top, leftMid, rect.Bottom).AddMargin(margin, margin, 0, margin), // left partition
+                new Rect(leftMid, rect.Top, rect.Right, rect.Bottom).AddMargin(margin), // right partition
+                verticalSplit
+            )
+            : (
+                new Rect(rect.Left, rect.Top, rect.Right, topMid).AddMargin(margin, margin, margin, 0), // top partition
+                new Rect(rect.Left, topMid, rect.Right, rect.Bottom).AddMargin(margin), // bottom partition
+                verticalSplit
+            );
+    }
+
+    public static Rect AddMargin(this Rect rect, int margin)
+        => rect.AddMargin(margin, margin, margin, margin);
+
+    public static Rect AddMargin(this Rect rect, int left, int top, int right, int bottom)
+        => new(rect.Left + left, rect.Top + top, rect.Right - right, rect.Bottom - bottom);
+
+    public static Rect AddPadding(this Rect rect, int padding)
+        => rect.AddPadding(padding, padding, padding, padding);
+
+    public static Rect AddPadding(this Rect rect, int left, int top, int right, int bottom)
+        => new(rect.Left - left, rect.Top - top, rect.Right + right, rect.Bottom + bottom);
 }
--- src/cswm/WindowManagement/WindowManagementOptions.cs
+++ src/cswm/WindowManagement/WindowManagementOptions.cs
@@ -2,7 +2,18 @@ namespace cswm.WindowManagement;
 
 public class WindowManagementOptions
 {
+    /// <summary>
+    /// If set, windows will be tracked but not managed.
+    /// </summary>
     public bool DoNotManage { get; init; }
+    
+    /// <summary>
+    /// Margin around all windows.
+    /// </summary>
     public int Margin { get; init; }
+
+    /// <summary>
+    /// Padding for each monitor.
+    /// </summary>
     public int Padding { get; init; }
 }
