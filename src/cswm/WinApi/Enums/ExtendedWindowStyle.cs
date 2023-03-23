34af02854442b20674d407359832bb01082d0cdd
ebalzuweit <ebalzuweit@gmail.com>
2023-03-23 02:06:51 -0500

    Add additional window styles, refactor style logging (#21)

--- src/cswm/WinApi/Enums/ExtendedWindowStyle.cs
+++ src/cswm/WinApi/Enums/ExtendedWindowStyle.cs
@@ -9,13 +9,29 @@ namespace cswm.WinApi;
 public enum ExtendedWindowStyle : long
 {
     /// <summary>
+    /// The window should be placed above all non-topmost windows and should stay above them, even when the window is deactivated. To add or remove this style, use the SetWindowPos function.
+    /// </summary>
+    WS_EX_TOPMOST = 0x00000008L,
+    /// <summary>
+    /// The window is intended to be used as a floating toolbar. A tool window has a title bar that is shorter than a normal title bar, and the window title is drawn using a smaller font. A tool window does not appear in the taskbar or in the dialog that appears when the user presses ALT+TAB. If a tool window has a system menu, its icon is not displayed on the title bar. However, you can display the system menu by right-clicking or by typing ALT+SPACE. 
+    /// </summary>
+    WS_EX_TOOLWINDOW = 0x00000080L,
+    /// <summary>
+    /// The window has a border with a raised edge.
+    /// </summary>
+    WS_EX_WINDOWEDGE = 0x00000100L,
+    /// <summary>
+    /// The window has a border with a sunken edge.
+    /// </summary>
+    WS_EX_CLIENTEDGE = 0x00000200L,
+    /// <summary>
     /// Forces a top-level window onto the taskbar when the window is visible.
     /// </summary>
     WS_EX_APPWINDOW = 0x00040000L,
     /// <summary>
-    /// The window is intended to be used as a floating toolbar. A tool window has a title bar that is shorter than a normal title bar, and the window title is drawn using a smaller font. A tool window does not appear in the taskbar or in the dialog that appears when the user presses ALT+TAB. If a tool window has a system menu, its icon is not displayed on the title bar. However, you can display the system menu by right-clicking or by typing ALT+SPACE.
+    /// The window is a layered window. This style cannot be used if the window has a class style of either CS_OWNDC or CS_CLASSDC. Windows 8: The WS_EX_LAYERED style is supported for top-level windows and child windows. Previous Windows versions support WS_EX_LAYERED only for top-level windows.
     /// </summary>
-    WS_EX_TOOLWINDOW = 0x00000080L,
+    WS_EX_LAYERED = 0x00080000,
     /// <summary>
     /// <para>A top-level window created with this style does not become the foreground window when the user clicks it. The system does not bring this window to the foreground when the user minimizes or closes the foreground window.</para>
     /// <para>The window should not be activated through programmatic access or via keyboard navigation by accessible technology, such as Narrator.</para>
@@ -23,4 +39,12 @@ public enum ExtendedWindowStyle : long
     /// <para>The window does not appear on the taskbar by default. To force the window to appear on the taskbar, use the WS_EX_APPWINDOW style.</para>
     /// </summary>
     WS_EX_NOACTIVATE = 0x08000000L,
+    /// <summary>
+    /// The window is an overlapped window.
+    /// </summary>
+    WS_EX_OVERLAPPEDWINDOW = (WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE),
+    /// <summary>
+    /// The window is palette window, which is a modeless dialog box that presents an array of commands. 
+    /// </summary>
+    WS_EX_PALETTEWINDOW = (WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST)
 }
--- src/cswm/WinApi/Enums/WindowStyle.cs
+++ src/cswm/WinApi/Enums/WindowStyle.cs
@@ -29,6 +29,22 @@ public enum WindowStyle : long
     /// </summary>
     WS_SYSMENU = 0x00080000L,
     /// <summary>
+    /// The window has a horizontal scroll bar.
+    /// </summary>
+    WS_HSCROLL = 0x00100000L,
+    /// <summary>
+    /// The window has a vertical scroll bar.
+    /// </summary>
+    WS_VSCROLL = 0x00200000L,
+    /// <summary>
+    /// The window has a border of a style typically used with dialog boxes. A window with this style cannot have a title bar.
+    /// </summary>
+    WS_DLGFRAME = 0x00400000L,
+    /// <summary>
+    /// The window has a thin-line border.
+    /// </summary>
+    WS_BORDER = 0x00800000L,
+    /// <summary>
     /// The window has a title bar (includes the WS_BORDER style).
     /// </summary>
     WS_CAPTION = 0x00C00000L,
@@ -40,6 +56,14 @@ public enum WindowStyle : long
     /// Excludes the area occupied by child windows when drawing occurs within the parent window. This style is used when creating the parent window.
     /// </summary>
     WS_CLIPCHILDREN = 0x02000000L,
+    /// <summary>
+    /// Clips child windows relative to each other; that is, when a particular child window receives a WM_PAINT message, the WS_CLIPSIBLINGS style clips all other overlapping child windows out of the region of the child window to be updated. If WS_CLIPSIBLINGS is not specified and child windows overlap, it is possible, when drawing within the client area of a child window, to draw within the client area of a neighboring child window.
+    /// </summary>
+    WS_CLIPSIBLINGS = 0x04000000L,
+    /// <summary>
+    /// The window is initially disabled. A disabled window cannot receive input from the user. To change this after a window has been created, use the EnableWindow function.
+    /// </summary>
+    WS_DISABLED = 0x08000000L,
     /// <summary>
     /// The window is initially minimized. Same as the WS_MINIMIZE style.
     /// </summary>
