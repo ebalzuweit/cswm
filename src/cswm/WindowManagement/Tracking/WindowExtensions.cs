dc34d3bfa06c0483886fe95c188c7e032145fe75
Evan Balzuweit <ebalzuweit@gmail.com>
2023-03-23 00:06:32 -0700

    Add additional window styles, refactor style logging

--- src/cswm/WindowManagement/Tracking/WindowExtensions.cs
+++ src/cswm/WindowManagement/Tracking/WindowExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using cswm.WinApi;
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Text;
 
@@ -7,9 +8,17 @@ namespace cswm.WindowManagement.Tracking;
 
 public static class WindowExtensions
 {
-    private static readonly Lazy<ImmutableDictionary<string, WindowStyle>> _styleCache =
-        new Lazy<ImmutableDictionary<string, WindowStyle>>(() =>
-            Enum.GetValues<WindowStyle>().ToImmutableDictionary(x => x.ToString()));
+    private static readonly Lazy<ImmutableArray<CachedStyle>> _styleCache =
+        new Lazy<ImmutableArray<CachedStyle>>(() =>
+        {
+            var cache = new List<CachedStyle>();
+            foreach (var ws in Enum.GetValues<WindowStyle>())
+                cache.Add(new(ws));
+            foreach (var exws in Enum.GetValues<ExtendedWindowStyle>())
+                cache.Add(new(exws));
+
+            return cache.ToImmutableArray();
+        });
 
     public static string GetDebugString(this Window window)
     {
@@ -17,12 +26,19 @@ public static class WindowExtensions
 
         var sb = new StringBuilder();
         sb.AppendLine(window.ToString());
+        sb.AppendLine($"Owner null: {User32.GetWindow(window.hWnd, GetWindowType.GW_OWNER)}");
         foreach (var (s, t) in _styleCache.Value)
         {
             sb.AppendLine($"{s}: {HasStyle(t)}");
         }
         return sb.ToString();
 
-        bool HasStyle(WindowStyle ws) => (style & (long)ws) != 0;
+        bool HasStyle(long ws) => (style & ws) != 0;
+    }
+
+    private record CachedStyle(string Style, long Value)
+    {
+        public CachedStyle(WindowStyle ws) : this(ws.ToString(), (long)ws) { }
+        public CachedStyle(ExtendedWindowStyle exws) : this(exws.ToString(), (long)exws) { }
     }
 }
