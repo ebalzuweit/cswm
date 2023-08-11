c78f00fcd80436cf7f6a637006bd31e5f1097923
ebalzuweit <ebalzuweit@gmail.com>
2023-08-10 19:26:14 -0700

    Refactored window tracking strategy

--- src/cswm/WindowManagement/Tracking/DefaultWindowTrackingStrategy.cs
+++ src/cswm/WindowManagement/Tracking/DefaultWindowTrackingStrategy.cs
@@ -1,26 +1,44 @@
-﻿using cswm.WinApi;
+﻿using System;
+using cswm.WinApi;
 
 namespace cswm.WindowManagement.Tracking;
 
 public class DefaultWindowTrackingStrategy : IWindowTrackingStrategy
 {
-    public bool ShouldTrack(Window window)
+    public bool ShouldTrack(Window window) => IsAltTabWindow(window);
+
+    private bool IsAltTabWindow(Window window)
     {
-        var hasOwner = (long)User32.GetWindow(window.hWnd, GetWindowType.GW_OWNER) != 0;
-        if (hasOwner)
+        if (window.hWnd == User32.GetWindow(window.hWnd, GetWindowType.GW_OWNER))
             return false;
 
-        var style = (long)User32.GetWindowLongPtr(window.hWnd, WindowLongFlags.GWL_STYLE);
-        if (HasStyle(WindowStyle.WS_CAPTION) == false ||
-            HasStyle(WindowStyle.WS_MINIMIZEBOX) == false ||
-            HasStyle(WindowStyle.WS_MAXIMIZEBOX) == false)
-            return false;
-        if (HasStyle(WindowStyle.WS_CLIPCHILDREN) == false &&
-            HasStyle(WindowStyle.WS_POPUP))
-            return false;
+        var root = User32.GetAncestor(window.hWnd, GetAncestorFlags.GA_ROOTOWNER);
+        if (GetLastVisibleActivePopup(root) == window.hWnd)
+        {
+            var exstyle = (long)User32.GetWindowLongPtr(window.hWnd, WindowLongFlags.GWL_EXSTYLE);
+            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_APPWINDOW))
+                return true;
+            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_TOOLWINDOW))
+                return false;
+            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_NOACTIVATE))
+                return false;
 
-        return true;
 
-        bool HasStyle(WindowStyle ws) => (style & (long)ws) != 0;
+            return true;
+        }
+        return false;
+
+        bool HasExStyle(long style, ExtendedWindowStyle ws) => (style & (long)ws) != 0;
+    }
+
+    private IntPtr GetLastVisibleActivePopup(IntPtr hwnd)
+    {
+        var last = User32.GetLastActivePopup(hwnd);
+        if (User32.IsWindowVisible(last)) // TODO: DWMWA_CLOAKED == false
+            return last;
+        else if (last == hwnd)
+            return IntPtr.Zero;
+        else
+            return GetLastVisibleActivePopup(last);
     }
 }
