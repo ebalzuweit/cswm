0b3aee9c1055f39a056b9e4cdb82e20cfd2e1cba
ebalzuweit <ebalzuweit@gmail.com>
2023-10-01 14:35:13 -0700

    Fix Window split calculation, re-enable tests (#50)
    * Fix aspectRatio calculation when splitting spaces
    
    * Update & re-enable arrangement tests

---
+++ test/cswm.Tests/Mocks.cs
@@ -0,0 +1,49 @@
+﻿using cswm.WinApi;
+using cswm.WindowManagement;
+using cswm.WindowManagement.Arrangement;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+
+namespace cswm.Tests;
+
+public static class Mocks
+{
+    public static ILogger<T> Logger<T>()
+    {
+        var mock = new Mock<ILogger<T>>();
+        return mock.Object;
+    }
+
+    public static Monitor[] GetMonitors(Rect size, int count = 1)
+    {
+        var monitors = new Monitor[count];
+        for (int i = 0; i < count; i++)
+        {
+            var mock = new Mock<Monitor>(IntPtr.Zero);
+            mock.Setup(monitor => monitor.WorkArea)
+                .Returns(size);
+            monitors[i] = mock.Object;
+        }
+        return monitors;
+    }
+
+    public static WindowLayout[] GetWindowLayouts(int count = 1)
+    {
+        var windows = new WindowLayout[count];
+        for (int i = 0; i < count; i++)
+        {
+            windows[i] = new WindowLayout(new Window((Windows.Win32.Foundation.HWND)IntPtr.Zero), new Rect());
+        }
+        return windows;
+    }
+
+    public static IOptions<WindowManagementOptions> WindowManagementOptions(WindowManagementOptions? options = null)
+    {
+        options ??= new();
+        var mock = new Mock<IOptions<WindowManagementOptions>>();
+        mock.Setup(x => x.Value).Returns(options);
+        return mock.Object;
+    }
+}
--- test/cswm.Tests/SplitArrangementStrategyTests.cs
+++ test/cswm.Tests/SplitArrangementStrategyTests.cs
@@ -1,100 +1,75 @@
+using cswm.Tests;
 using cswm.WinApi;
-using cswm.WindowManagement;
-using Moq;
-using System;
+using cswm.WindowManagement.Arrangement;
+using System.Linq;
+using Xunit;
 
 public class SplitArrangementStrategyTests
 {
-    //[Fact]
-    //public void Arrange_OneWindow_FillsMonitor()
-    //{
-    //	var strategy = new SplitArrangementStrategy();
-    //	var windows = GetWindowLayouts(1);
-    //	var monitor = GetMonitors(1).First();
-    //	var monitorLayout = new MonitorLayout(monitor.hMonitor, monitor.WorkArea, windows, monitor.Ratio);
+    private SplitArrangementStrategy Strategy => new(Mocks.Logger<SplitArrangementStrategy>(), Mocks.WindowManagementOptions());
+    private Rect MonitorSize => new(0, 0, 1920, 1080);
 
-    //	var layouts = strategy.Arrange(new[] { monitorLayout });
-    //	var positions = layouts.Select(l => l.Position);
-
-    //	Assert.NotNull(layouts);
-    //	Assert.Single(layouts);
-    //	Assert.Contains(new Rect(0, 0, 1920, 1027), positions);
-    //}
-
-    //[Fact]
-    //public void Arrange_TwoWindows_1To1Split()
-    //{
-    //	var strategy = new SplitArrangementStrategy();
-    //	var windows = GetWindowLayouts(2);
-    //	var monitor = GetMonitors(1).First();
-    //	var monitorLayout = new MonitorLayout(monitor.hMonitor, monitor.WorkArea, windows, monitor.Ratio);
-
-    //	var layouts = strategy.Arrange(new[] { monitorLayout });
-    //	var positions = layouts.Select(l => l.Position);
-
-    //	Assert.NotNull(layouts);
-    //	Assert.Equal(2, layouts.Count());
-    //	Assert.Contains(new Rect(0, 0, 960, 1027), positions);
-    //	Assert.Contains(new Rect(960, 0, 1920, 1027), positions);
-    //}
-
-    //[Fact]
-    //public void Arrange_ThreeWindows_2To1To1Split()
-    //{
-    //	var strategy = new SplitArrangementStrategy();
-    //	var windows = GetWindowLayouts(3);
-    //	var monitor = GetMonitors(1).First();
-    //	var monitorLayout = new MonitorLayout(monitor.hMonitor, monitor.WorkArea, windows, monitor.Ratio);
-
-    //	var layouts = strategy.Arrange(new[] { monitorLayout });
-    //	var positions = layouts.Select(l => l.Position);
-
-    //	Assert.NotNull(layouts);
-    //	Assert.Equal(3, layouts.Count());
-    //	Assert.Contains(new Rect(0, 0, 960, 1027), positions);
-    //	Assert.Contains(new Rect(960, 0, 1920, 514), positions);
-    //	Assert.Contains(new Rect(960, 514, 1920, 1027), positions);
-    //}
-
-    //[Fact]
-    //public void Arrange_FourWindows_4To2To1To1Split()
-    //{
-    //	var strategy = new SplitArrangementStrategy();
-    //	var windows = GetWindowLayouts(4);
-    //	var monitor = GetMonitors(1).First();
-    //	var monitorLayout = new MonitorLayout(monitor.hMonitor, monitor.WorkArea, windows, monitor.Ratio);
-
-    //	var layouts = strategy.Arrange(new[] { monitorLayout });
-    //	var positions = layouts.Select(l => l.Position);
-
-    //	Assert.NotNull(layouts);
-    //	Assert.Equal(4, layouts.Count());
-    //	Assert.Contains(new Rect(0, 0, 960, 1027), positions);
-    //	Assert.Contains(new Rect(960, 0, 1920, 514), positions);
-    //	Assert.Contains(new Rect(960, 514, 1440, 1027), positions);
-    //	Assert.Contains(new Rect(1440, 514, 1920, 1027), positions);
-    //}
-
-    // private WindowLayout[] GetWindowLayouts(int n)
-    // {
-    //     var windows = new WindowLayout[n];
-    //     for (int i = 0; i < n; i++)
-    //     {
-    //         windows[i] = new WindowLayout(new Window(IntPtr.Zero), new Rect());
-    //     }
-    //     return windows;
-    // }
-
-    private Monitor[] GetMonitors(int n)
+    [Fact]
+    public void Arrange_OneWindow_FillsMonitor()
     {
-        var monitors = new Monitor[n];
-        for (int i = 0; i < n; i++)
-        {
-            var mock = new Mock<Monitor>(IntPtr.Zero);
-            mock.Setup(monitor => monitor.WorkArea)
-                .Returns(new Rect(0, 0, 1920, 1027));
-            monitors[i] = mock.Object;
-        }
-        return monitors;
+        var windows = Mocks.GetWindowLayouts(1);
+        var monitor = Mocks.GetMonitors(MonitorSize).First();
+        var monitorLayout = new MonitorLayout(monitor, windows);
+
+        var layout = Strategy.Arrange(monitorLayout);
+
+        Assert.NotNull(layout);
+        Assert.Equal(new Rect(0, 0, 1920, 1080), layout.Windows.First().Position);
+    }
+
+    [Fact]
+    public void Arrange_TwoWindows_1To1Split()
+    {
+        var windows = Mocks.GetWindowLayouts(2);
+        var monitor = Mocks.GetMonitors(MonitorSize).First();
+        var monitorLayout = new MonitorLayout(monitor, windows);
+
+        var layout = Strategy.Arrange(monitorLayout);
+        var positions = layout.Windows.Select(x => x.Position);
+
+        Assert.NotNull(layout);
+        Assert.Equal(2, positions.Count());
+        Assert.Contains(new Rect(0, 0, 960, 1080), positions);
+        Assert.Contains(new Rect(960, 0, 1920, 1080), positions);
+    }
+
+    [Fact]
+    public void Arrange_ThreeWindows_2To1To1Split()
+    {
+        var windows = Mocks.GetWindowLayouts(3);
+        var monitor = Mocks.GetMonitors(MonitorSize).First();
+        var monitorLayout = new MonitorLayout(monitor, windows);
+
+        var layouts = Strategy.Arrange(monitorLayout);
+        var positions = layouts.Windows.Select(x => x.Position);
+
+        Assert.NotNull(layouts);
+        Assert.Equal(3, positions.Count());
+        Assert.Contains(new Rect(0, 0, 960, 1080), positions);
+        Assert.Contains(new Rect(960, 0, 1920, 540), positions);
+        Assert.Contains(new Rect(960, 540, 1920, 1080), positions);
+    }
+
+    [Fact]
+    public void Arrange_FourWindows_4To2To1To1Split()
+    {
+        var windows = Mocks.GetWindowLayouts(4);
+        var monitor = Mocks.GetMonitors(MonitorSize).First();
+        var monitorLayout = new MonitorLayout(monitor, windows);
+
+        var layout = Strategy.Arrange(monitorLayout);
+        var positions = layout.Windows.Select(x => x.Position);
+
+        Assert.NotNull(layout);
+        Assert.Equal(4, positions.Count());
+        Assert.Contains(new Rect(0, 0, 960, 1080), positions);
+        Assert.Contains(new Rect(960, 0, 1920, 540), positions);
+        Assert.Contains(new Rect(960, 540, 1440, 1080), positions);
+        Assert.Contains(new Rect(1440, 540, 1920, 1080), positions);
     }
 }
