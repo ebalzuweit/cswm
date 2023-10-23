943d8709dfc469821577c21a8869e3d7434aa78b
ebalzuweit <ebalzuweit@gmail.com>
2023-10-23 13:38:27 -0700

    Add BspTree tests

--- test/cswm.Tests/Arrangement/Partitioning/BspTreeTests.cs
+++ test/cswm.Tests/Arrangement/Partitioning/BspTreeTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using cswm.Arrangement;
+﻿using cswm.Arrangement.Partitioning;
 using cswm.WinApi;
 using Xunit;
 
@@ -10,5 +8,11 @@ public class BspTreeTests
 {
 	public Rect MonitorSize => new(0, 0, 1920, 1080);
 
-	// GetEnumerator
+	[Fact]
+	public void IsLeaf_ReturnsTrue_IfPartitionNull()
+	{
+		var node = new BspTree();
+
+		Assert.True(node.IsLeaf);
+	}
 }
