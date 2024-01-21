5cd7b3c827c9d012fd01b0641b48f3c66ee73e2e
ebalzuweit <ebalzuweit@gmail.com>
2024-01-21 10:52:47 -0800

    Update window tracking strategy

--- src/cswm/Tracking/DefaultWindowTrackingStrategy.cs
+++ src/cswm/Tracking/DefaultWindowTrackingStrategy.cs
@@ -1,55 +1,62 @@
 ﻿using cswm.WinApi;
-using System;
+using Windows.Win32;
+using Windows.Win32.UI.WindowsAndMessaging;
 
 namespace cswm.Tracking;
 
 public class DefaultWindowTrackingStrategy : IWindowTrackingStrategy
 {
-    public bool ShouldTrack(Window window) => IsAltTabWindow(window);
-
-    /// <remarks>
-    /// Based on <see href="https://devblogs.microsoft.com/oldnewthing/20071008-00/?p=24863"/>
-    /// </remarks>
-    private bool IsAltTabWindow(Window window)
+    public bool ShouldTrack(Window window)
     {
-        if (window.hWnd == User32.GetWindow(window.hWnd, GetWindowType.GW_OWNER))
-            return false;
-
-        var root = User32.GetAncestor(window.hWnd, GetAncestorFlags.GA_ROOTOWNER);
-        if (GetLastVisibleActivePopup(root) == window.hWnd)
+        // no GW_OWNER
+        var owner = PInvoke.GetWindow(window.hWnd, GET_WINDOW_CMD.GW_OWNER);
+        if (owner == window.hWnd)
         {
-            // Window must be min/max-imizable
-            var style = (long)User32.GetWindowLongPtr(window.hWnd, WindowLongFlags.GWL_STYLE);
-            if (HasStyle(style, WindowStyle.WS_MAXIMIZEBOX) == false)
-                return false;
-            if (HasStyle(style, WindowStyle.WS_MINIMIZEBOX) == false)
-                return false;
+            return false;
+        }
 
-            // Follow extended window styling rules
-            var exstyle = (long)User32.GetWindowLongPtr(window.hWnd, WindowLongFlags.GWL_EXSTYLE);
-            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_APPWINDOW))
-                return true;
-            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_TOOLWINDOW))
-                return false;
-            if (HasExStyle(exstyle, ExtendedWindowStyle.WS_EX_NOACTIVATE))
-                return false;
+        if (PInvoke.IsWindowVisible(window.hWnd) == false)
+        {
+            return false;
+        }
 
+        // Note: All exclusion rules must be above this line
+
+        var style = PInvoke.GetWindowLong(window.hWnd, WINDOW_LONG_PTR_INDEX.GWL_STYLE);
+        var exStyle = PInvoke.GetWindowLong(window.hWnd, WINDOW_LONG_PTR_INDEX.GWL_EXSTYLE);
+
+        // respect extended styling rules
+        if (HasExStyle(exStyle, ExtendedWindowStyle.WS_EX_TOOLWINDOW) || HasExStyle(exStyle, ExtendedWindowStyle.WS_EX_NOACTIVATE))
+        {
+            return false;
+        }
+        if (HasExStyle(exStyle, ExtendedWindowStyle.WS_EX_APPWINDOW))
+        {
             return true;
         }
+
+        if (HasStyle(style, WindowStyle.WS_POPUP) || HasExStyle(exStyle, ExtendedWindowStyle.WS_EX_CLIENTEDGE) || HasExStyle(exStyle, 0x00000001 /* WS_EX_DLGMODALFRAME */))
+        {
+            return false;
+        }
+
+        // Top-level window
+        if (window.hWnd == PInvoke.GetAncestor(window.hWnd, GET_ANCESTOR_FLAGS.GA_ROOT))
+        {
+            return true;
+        }
+
+        // WS_OVERLAPPEDWINDOW suggests true, but some windows lie
+        //if (HasStyle(style, WindowStyle.WS_OVERLAPPEDWINDOW))
+        //{
+        //    return true;
+        //}
+
         return false;
-
-        bool HasStyle(long style, WindowStyle ws) => (style & (long)ws) != 0;
-        bool HasExStyle(long style, ExtendedWindowStyle ws) => (style & (long)ws) != 0;
     }
 
-    private IntPtr GetLastVisibleActivePopup(IntPtr hwnd)
-    {
-        var last = User32.GetLastActivePopup(hwnd);
-        if (User32.IsWindowVisible(last))
-            return last;
-        else if (last == hwnd)
-            return IntPtr.Zero;
-        else
-            return GetLastVisibleActivePopup(last);
-    }
+    private static bool HasStyle(int style, WindowStyle ws) => (style & (int)ws) != 0;
+    private static bool HasExStyle(int exStyle, ExtendedWindowStyle ws) => (exStyle & (int)ws) != 0;
+    private static bool HasExStyle(int exStyle, int ws) => (exStyle & ws) != 0;
+
 }
