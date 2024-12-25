2d71d1daad90326f9990679e9f1775abf76cd4d0
ebalzuweit <ebalzuweit@gmail.com>
2024-12-25 10:37:51 -0600

    Use dispose pattern

--- src/cswm.Windows/Services/WindowsWindowEventHook.cs
+++ src/cswm.Windows/Services/WindowsWindowEventHook.cs
@@ -5,15 +5,12 @@ namespace cswm.Windows.Services;
 
 public class WindowsWindowEventHook : IWindowEventHook
 {
+	private bool disposedValue;
+
 	public event EventHandler<WindowInfo>? WindowCreated;
 	public event EventHandler<WindowInfo>? WindowDestroyed;
 	public event EventHandler<WindowInfo>? WindowMoved;
 	public event EventHandler<WindowInfo>? WindowStateChanged;
-
-	public void Dispose()
-	{
-		throw new NotImplementedException();
-	}
 
 	public void StartSubscriptions()
 	{
@@ -23,5 +20,34 @@ public class WindowsWindowEventHook : IWindowEventHook
 	public void StopSubscriptions()
 	{
 		throw new NotImplementedException();
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (!disposedValue)
+		{
+			if (disposing)
+			{
+				// TODO: dispose managed state (managed objects)
+			}
+
+			// TODO: free unmanaged resources (unmanaged objects) and override finalizer
+			// TODO: set large fields to null
+			disposedValue = true;
+		}
+	}
+
+	// // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+	// ~WindowsWindowEventHook()
+	// {
+	//     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+	//     Dispose(disposing: false);
+	// }
+
+	public void Dispose()
+	{
+		// Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+		Dispose(disposing: true);
+		GC.SuppressFinalize(this);
 	}
 }
