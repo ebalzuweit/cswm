2534c46ac87c3a7bc63b2b58f5273959b70c0aef
ebalzuweit <ebalzuweit@gmail.com>
2025-01-01 14:10:06 -0800

    Add basic macOS status bar icon

--- src/cswm.macOS.TrayApp/AppDelegate.cs
+++ src/cswm.macOS.TrayApp/AppDelegate.cs
@@ -1,18 +1,41 @@
 using AppKit;
+using cswm.Core;
 using Foundation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace cswm.macOS.TrayApp;
 
 [Register("AppDelegate")]
 public class AppDelegate : NSApplicationDelegate
 {
+    private IHost? host;
+    private CswmStatusBarItem? statusBarItem;
+
     public override void DidFinishLaunching(NSNotification notification)
     {
-        // Insert code here to initialize your application
+        host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => services
+                .AddCswmCoreServices()
+                .AddCswmMacOsServices()
+                .AddTransient<CswmStatusBarItem>()
+            )
+            .Build();
+
+        host.Start();
+
+        statusBarItem = host.Services.GetRequiredService<CswmStatusBarItem>();
+        statusBarItem.AddToStatusBar();
     }
 
     public override void WillTerminate(NSNotification notification)
     {
-        // Insert code here to tear down your application
+        statusBarItem = null;
+        if (host is not null)
+        {
+            host?.StopAsync().Wait();
+            host?.Dispose();
+            host = null;
+        }
     }
 }
